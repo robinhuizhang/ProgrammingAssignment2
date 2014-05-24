3773a377d8c16ed1d4114cc1f1e3c95facb7cbbe
Robin <robinhuizhang@gmail.com>
2014-05-24 08:59:20 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,24 @@
 # This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+ #initialize
   m <- NULL
+#assign a value to an object in an environment that is different from the current   
+  set <-# This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+ #initialize
+  m <- NULL
+#assign a value to an object in an environment that is different from the current   
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+#getValue
   get <- function() x
+#set inverse of a matrix
   setInv <- function(ginv) m <<- ginv
+#get inverse of a matrix
   getInv <- function() m
   list(set = set, get = get,
        setInv = setInv,
@@ -19,11 +30,47 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
+ #get inverse of the function
   m <- x$getInv()
+  # if m doesn't exist, print message and return m
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  # if m does exist, compute
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setInv(m)
+  m 
+}
+ function(y) {
+    x <<- y
+    m <<- NULL
+  }
+#getValue
+  get <- function() x
+#set inverse of a matrix
+  setInv <- function(ginv) m <<- ginv
+#get inverse of a matrix
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+
+}
+
+
+## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...) {
+ #get inverse of the function
+  m <- x$getInv()
+  # if m doesn't exist, print message and return m
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # if m does exist, compute
   data <- x$get()
   m <- ginv(data, ...)
   x$setInv(m)
